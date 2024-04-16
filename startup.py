db7e6482a11d804a1c72308968b6507cd250ec5a
skittlexyz <mo644774@gmail.com>
2024-04-16 14:46:53 -0300

    Added info display at startup

--- startup.py
+++ startup.py
@@ -1,10 +1,18 @@
 import time
 import os
+import socket
 
 time.sleep(10)
 
 if os.name == 'nt': os.system('cls')
 else: os.system('clear')
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    s.connect(("8.8.8.8", 80))
+    internal_ip = s.getsockname()[0]
+except:
+    internal_ip = "not connected"
 
 banner = """
     ██████╗  ██████╗ ██╗   ██╗███████╗██████╗ 
@@ -15,4 +23,8 @@ banner = """
     ╚═╝  ╚═╝ ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝
 
                                     OBR 2024"""
+info = f"""
+    Private IP: {internal_ip}
+"""
 print(banner)
+print(info)
